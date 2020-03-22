bc29bed47e856105515adb5caaad405e44436589
sja119 <sja119@ic.ac.uk>
2020-03-22 13:56:56 +0000

    perfect

--- colorGame.js
+++ colorGame.js
@@ -50,6 +50,9 @@ var game = function() {
             colorArray = colorArray.map(color => correctColor)
             assignColors()
             document.getElementById("header").style.backgroundColor = correctColor
+            changeBarTextColor(correctColor)
+            setHoverListener(correctColor)
+            document.getElementById("new_game").innerText = "Play Again?"
 
         } else {
             message("Try Again :(")
@@ -60,7 +63,7 @@ var game = function() {
 }
 
 function gameModeClickListeners() {
-    buttons = document.querySelectorAll("li#modes>button")
+    buttons = document.querySelectorAll("#modes>button")
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function() {
             for (var j = 0; j < buttons.length; j++) {
@@ -76,6 +79,21 @@ function newGameClickListeners() {
     document.getElementById("new_game").addEventListener("click", newGame)
 }
 
+function changeBarTextColor(newColor) {
+    var elements = document.querySelectorAll("#bar, #bar button")
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.color = newColor;
+        elements[i].style.backgroundColor = "white"
+        elements[i].style.borderColor = "white"
+    }
+
+    document.querySelector("#bar #modes button:active, #modes .btn-light:not(:disabled):not(.disabled).active, #modes .btn-light:not(:disabled):not(.disabled):active, #modes .show>.btn-light.dropdown-toggle").style.backgroundColor = newColor
+    document.querySelector("#bar #modes button:active, #modes .btn-light:not(:disabled):not(.disabled).active, #modes .btn-light:not(:disabled):not(.disabled):active, #modes .show>.btn-light.dropdown-toggle").style.borderColor = newColor
+    document.querySelector("#bar #modes button:active, #modes .btn-light:not(:disabled):not(.disabled).active, #modes .btn-light:not(:disabled):not(.disabled):active, #modes .show>.btn-light.dropdown-toggle").style.color = "white"
+
+
+}
+
 var gameMode = "easy"
 var colorArray = undefined
 var correctColor = undefined
@@ -83,7 +101,9 @@ var pickedColor = undefined
 
 function newGame() {
     message("")
-    document.getElementById("header").style.backgroundColor = "rgb(66, 135, 245)"
+    document.getElementById("header").style.backgroundColor = "rgb(79, 140, 196)"
+    changeBarTextColor("rgb(79, 140, 196)")
+    document.getElementById("new_game").innerText = "New Game"
     generateRandomColors(gameMode === "easy" ? 3 : 6)
     correctColor = colorArray[pickRandomColor()]
     setCorrectColor()
